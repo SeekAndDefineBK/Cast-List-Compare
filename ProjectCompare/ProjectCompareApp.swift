209a275e95e99ad4a0809016f4944358100660f5
Brett Koster <skdfmanagement@icloud.com>
2024-06-25 15:15:00 -0500

    Initial Commit

---
+++ ProjectCompare/ProjectCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectCompareApp.swift
+//  ProjectCompare
+//
+//  Created by Brett Koster on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
