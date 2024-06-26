cc53d4332111890b0c413cd26a79a8efbc6698e5
Brett Koster <skdfmanagement@icloud.com>
2024-06-25 23:08:17 -0500

    Create TMDBKeyProtocol.swift

---
+++ ProjectCompare/Service/TMDBKeyProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  TMDBKeyProtocol.swift
+//  ProjectCompare
+//
+//  Created by Brett Koster on 6/25/24.
+//
+
+import Foundation
+
+protocol TMDBKeyProtocol {
+    static var key: String { get }
+}
