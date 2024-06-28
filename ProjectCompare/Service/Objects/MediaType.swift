f5f7f0c2d3374adc2e239a9b0fd5eb3250c194be
Brett Koster <60300571+SeekAndDefineBK@users.noreply.github.com>
2024-06-27 22:27:06 -0500

    Create MediaType.swift

---
+++ ProjectCompare/Service/Objects/MediaType.swift
@@ -0,0 +1,13 @@
+//
+//  MediaType.swift
+//  ProjectCompare
+//
+//  Created by bk on 6/27/24.
+//
+
+import Foundation
+
+/// Convenience enum to prevent checking string values
+enum MediaType: String {
+    case tv, movie, unknown
+}
