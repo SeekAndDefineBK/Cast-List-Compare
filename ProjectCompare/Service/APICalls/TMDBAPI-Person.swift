7afbe090fbc6a61ffb4cf2ec9a993424bae4bfcc
Brett Koster <60300571+SeekAndDefineBK@users.noreply.github.com>
2024-06-27 22:25:43 -0500

    Reorganized and added documentation
    Organized into Person and Credits
    
    Added documentation about what each function does

--- ProjectCompare/Service/APICalls/TMDBAPI-Person.swift
+++ ProjectCompare/Service/APICalls/TMDBAPI-Person.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 extension TMDBAPI {
-    // MARK: Search with Query
-    func searchForPerson(with query: String, pageNumber: Int) async -> [Person]? {
+    // MARK: Person Search and Decoding
+    
+    /// Search TMDB API for a person with a user generated query
+    /// - Parameters:
+    ///   - query: String. The text the user types in.
+    ///   - pageNumber: Optional Int. Allows you to paginate through API results
+    /// - Returns: Optional Array of Person
+    func searchForPerson(with query: String, pageNumber: Int = 1) async -> [Person]? {
         // convert user query into string value compatible with url scheme
         let urlQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         
@@ -27,41 +33,10 @@ extension TMDBAPI {
             return nil
         }
     }
-            
-    // MARK: Get Person Credits
-    func getCredits(for person: Person) async -> CombinedCreditsJSON? {
-        // build url string with Person ID
-        let urlString = "https://api.themoviedb.org/3/person/\(person.id)/combined_credits?api_key=\(key)"
-        
-        do {
-            // use urlString to perform api call
-            let data = try await performAPICall(with: urlString)
-            
-            return decodeCombinedCredits(with: data)
-        } catch {
-            print("Failed to collect combined credits for \(person.name). ID: \(person.id). urlString: \(urlString)")
-            return nil
-        }
-    }
     
-    // MARK:
-    private func decodeCombinedCredits(with data: Data) -> CombinedCreditsJSON? {
-        // Initialize Decoder
-        let decoder = JSONDecoder()
-        
-        do {
-            // Convert data to CombinedCreditsJSON
-            let results = try decoder.decode(CombinedCreditsJSON.self, from: data)
-            
-            return results
-        } catch {
-            // Print raw error if this occurs and return nothing
-            print("Failed to convert data to CombinedCreditsJSON. Error \(error)")
-            return nil
-        }
-    }
-    
-    //MARK: Convert Data to Person Array
+    /// Converts Data object to an Array of Person
+    /// - Parameter data: Data received from cache or API Call
+    /// - Returns: Array of Person
     private func convertDataToPersonArray(with data: Data) -> [Person] {
         // Initialize Decoder
         let decoder = JSONDecoder()
@@ -79,4 +54,55 @@ extension TMDBAPI {
             return []
         }
     }
+            
+    // MARK: Get Person Credits
+    func getCredits(for person: Person) async -> [CombinedCredits]? {
+        // fetch credits from TMDB
+        let creditsJSON = await fetchCredits(for: person)
+        
+        // merge cast and crew list
+        
+        return (creditsJSON?.cast ?? []) + (creditsJSON?.crew ?? [])
+    }
+    
+    
+    /// Call TMDB API to get credits with a given Person
+    /// - Parameter person: Person. The object you want to find credits for.
+    /// - Returns: CombinedCreditsJSON. An object that contains all credits for this person
+    private func fetchCredits(for person: Person) async -> CombinedCreditsJSON? {
+        // build url string with Person ID
+        let urlString = "https://api.themoviedb.org/3/person/\(person.id)/combined_credits?api_key=\(key)"
+        
+        do {
+            // use urlString to perform api call
+            let data = try await performAPICall(with: urlString)
+            
+            return decodeCombinedCredits(with: data)
+        } catch {
+            print("Failed to collect combined credits for \(person.name). ID: \(person.id). urlString: \(urlString)")
+            return nil
+        }
+    }
+    
+    
+    /// Converts Data object to optional CombinedCreditsJSON object
+    /// - Parameter data: Data received from cache or API Call
+    /// - Returns: Optional CombinedCreditsJSON object
+    private func decodeCombinedCredits(with data: Data) -> CombinedCreditsJSON? {
+        // Initialize Decoder
+        let decoder = JSONDecoder()
+        
+        do {
+            // Convert data to CombinedCreditsJSON
+            let results = try decoder.decode(CombinedCreditsJSON.self, from: data)
+            
+            return results
+        } catch {
+            // Print raw error if this occurs and return nothing
+            print("Failed to convert data to CombinedCreditsJSON. Error \(error)")
+            return nil
+        }
+    }
+    
+
 }
