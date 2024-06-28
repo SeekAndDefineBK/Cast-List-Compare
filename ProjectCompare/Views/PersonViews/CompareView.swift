d88fb5c16ecd7c01598b26f1e2a88d0300de8ddf
Brett Koster <60300571+SeekAndDefineBK@users.noreply.github.com>
2024-06-27 22:29:22 -0500

    Initial CompareView prototype

---
+++ ProjectCompare/Views/PersonViews/CompareView.swift
@@ -0,0 +1,95 @@
+//
+//  CompareView.swift
+//  ProjectCompare
+//
+//  Created by bk on 6/27/24.
+//
+
+import SwiftUI
+
+struct CompareView: View {
+    @StateObject private var viewModel: CompareViewModel
+    
+    init(person1: Person, person2: Person) {
+        let model = CompareViewModel(person1: person1, person2: person2)
+        _viewModel = StateObject(wrappedValue: model)
+    }
+    
+    var body: some View {
+        VStack {
+            // MARK: Summarize the data for the user
+            HStack {
+                Text(viewModel.person1.name)
+                
+                Text(viewModel.person2.name)
+            }
+            
+            Text("\(viewModel.person1.name) and \(viewModel.person2.name) have been in \(viewModel.getPlural()) together")
+            
+            
+            // MARK: Give the user the rest of the details
+            List {
+                Section("Movies") {
+                    ForEach(viewModel.sharedCredits?.movieCredits ?? []) {
+                        Text($0.title)
+                    }
+                }
+                
+                Section("TV") {
+                    ForEach(viewModel.sharedCredits?.tvCredits ?? []) {
+                        Text($0.title)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension CompareView {
+    @MainActor
+    class CompareViewModel: ObservableObject {
+        // MARK: ViewModel Properties
+        @Published var person1: Person
+        @Published var person2: Person
+        
+        let tmdb = TMDBAPI.shared
+        
+        @Published var sharedCredits: SharedCreditsContainer? = nil
+        
+        // MARK: ViewModel Initializers
+        init(person1: Person, person2: Person) {
+            _person1 = Published(wrappedValue: person1)
+            _person2 = Published(wrappedValue: person2)
+            
+            Task {
+                await getCredits()
+            }
+        }
+                
+        // MARK: ViewModel Methods
+        
+        /// Fetches credits from tmdbapi and builds sharedCredits property
+        func getCredits() async {
+            let person1Credits = await tmdb.getCredits(for: person1)
+            let person2Credits = await tmdb.getCredits(for: person2)
+            
+            guard let person1Credits = person1Credits, let person2Credits = person2Credits else {
+                return
+            }
+            
+            sharedCredits = SharedCreditsContainer(
+                person1: person1,
+                person2: person2,
+                person1Credits: person1Credits,
+                person2Credits: person2Credits
+            )
+        }
+        
+        // Convenience phrasing constructor for if production should be plural
+        func getPlural() -> String {
+            let count = sharedCredits?.sharedCredits.count ?? 0
+            
+            return "\(count) production\(count == 0 || count > 2 ? "s" : "")"
+        }
+    }
+}
