d84cdccc7c62497285e46869266221b04324d292
Brett Koster <60300571+SeekAndDefineBK@users.noreply.github.com>
2024-06-28 23:07:10 -0500

    Styling - Search is on bottom, padding, GroupBox for search

--- ProjectCompare/Views/SearchPersonView.swift
+++ ProjectCompare/Views/SearchPersonView.swift
@@ -16,35 +16,68 @@ struct SearchPersonView: View {
     @Binding var selectedPerson: Person?
     
     var body: some View {
-        VStack {
-            TextField("Enter Person Name", text: $query)
-                .textFieldStyle(.roundedBorder)
-            
-            Button("Search") {
-                performSearch()
-            }
-            
+        ZStack(alignment: .bottom) {
             List {
-                ForEach(searchResults) { person in
-                    Button {
-                        withAnimation {
-                            selectedPerson = person
-                            dismiss()
+                Section {
+                    ForEach(searchResults) { person in
+                        Button {
+                            withAnimation {
+                                selectedPerson = person
+                                dismiss()
+                            }
+                        } label: {
+                            PersonProfileView(person: person)
                         }
-                    } label: {
-                        PersonProfileView(person: person)
+                        .foregroundStyle(.foreground)
                     }
-                    .foregroundStyle(.foreground)
+                    
+                } header: {
+                    Text(getPlural())
+                } footer: {
+                    if !searchResults.isEmpty {
+                        HStack {
+                            Image("TMDBLogo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 30)
+                            
+                            Text("Search results provided by The Movie Database")
+                                .padding(.horizontal)
+                        }
+                        .padding(.vertical)
+                        .padding(.bottom, 200) // in iOS 16 this adds extra space to the end to prevent the searchbox from overlapping the last item
+                    }
                 }
+                
             }
             
-            Text("Search result attribution")
+            GroupBox {
+                Button("Search") {
+                    performSearch()
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.vertical, 5) // this evenly places the search button from the search box
+                
+                TextField("Enter Person Name", text: $query)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.bottom, 40)
+            }
+            .backgroundStyle(.ultraThinMaterial)
+            
         }
+        .ignoresSafeArea(edges: .bottom)
+        
     }
     
     func performSearch() {
         Task {
             searchResults = await tmdb.searchForPerson(with: query, pageNumber: 1) ?? []
         }
+    }
+    
+    func getPlural() -> String {
+        let count = searchResults.count
+        
+        return "\(count) Result\(count == 0 || count > 2 ? "s" : "")"
     }
 }
