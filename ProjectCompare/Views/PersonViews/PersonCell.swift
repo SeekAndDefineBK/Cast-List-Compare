7cee58ff4de7faea6b69d26e9fd51651deb25ab7
Brett Koster <60300571+SeekAndDefineBK@users.noreply.github.com>
2024-06-28 22:32:43 -0500

    Create PersonCell.swift
    Shared view to display a persons profile and allow the user to clear its selection

---
+++ ProjectCompare/Views/PersonViews/PersonCell.swift
@@ -0,0 +1,54 @@
+//
+//  PersonCell.swift
+//  ProjectCompare
+//
+//  Created by bk on 6/28/24.
+//
+
+import SwiftUI
+
+struct PersonCell: View {
+    @StateObject private var viewModel: PersonCellModel
+    
+    init(person: Person, clearSelectedPerson: @escaping () -> Void) {
+        let model = PersonCellModel(
+            person: person,
+            clearSelectedPerson: clearSelectedPerson
+        )
+        _viewModel = StateObject(wrappedValue: model)
+    }
+    
+    var body: some View {
+        HStack {
+            PersonProfileView(person: viewModel.person)
+            
+            Spacer()
+            
+            Button {
+                viewModel.clearSelectedPerson()
+            } label: {
+                Label("Clear", systemImage: "x.circle")
+            }
+        }
+    }
+}
+
+extension PersonCell {
+    class PersonCellModel: ObservableObject {
+        // MARK: ViewModel Properties
+        let person: Person
+        let tmdb = TMDBAPI.shared
+        let clearSelectedPerson: () -> Void
+        
+        // MARK: ViewModel Initializers
+        init(person: Person, clearSelectedPerson: @escaping () -> Void) {
+            self.person = person
+            self.clearSelectedPerson = clearSelectedPerson
+        }
+                
+        // MARK: ViewModel Methods
+        func getImageRUL() -> URL? {
+            tmdb.getImageURL(with: person.profile_path ?? "")
+        }
+    }
+}
