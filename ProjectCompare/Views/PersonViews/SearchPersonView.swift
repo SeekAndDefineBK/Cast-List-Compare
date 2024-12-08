40178ad3bfe932ea871d08d1e72e3726793d5d67
Brett Koster <skdfmanagement@icloud.com>
2024-12-07 20:57:43 -0600

    Added model context, componentized search button, added view model

--- ProjectCompare/Views/PersonViews/SearchPersonView.swift
+++ ProjectCompare/Views/PersonViews/SearchPersonView.swift
@@ -6,55 +6,88 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SearchPersonView: View {
+    @Environment(\.modelContext) var modelContext
     @Environment(\.dismiss) var dismiss
     @Environment(\.isFocused) var isFocused
-    let tmdb = TMDBAPI.shared
     
-    @State private var query: String = ""
-    @State private var searchResults: [Person] = []
     @Binding var selectedPerson: Person?
     
     @FocusState private var keyboardFocused: Bool
+    
+    @State private var vm = viewModel()
+    @Query var pastPeople: [Person]
+    
+    @ViewBuilder
+    func SelectPersonButton(_ person: Person, fromHistory: Bool) -> some View {
+        Button {
+            withAnimation {
+                selectedPerson = person
+                
+                if !fromHistory {
+                    modelContext.insert(person)
+                    try? modelContext.save()
+                    
+                }
+                
+                dismiss()
+            }
+        } label: {
+            PersonProfileView(person: person)
+        }
+        .foregroundStyle(.foreground)
+    }
 
     var body: some View {
         ZStack(alignment: .bottom) {
             List {
-                Section {
-                    ForEach(searchResults) { person in
-                        Button {
-                            withAnimation {
-                                selectedPerson = person
-                                dismiss()
-                            }
-                        } label: {
-                            PersonProfileView(person: person)
+                if vm.searchResults.isEmpty {
+                    Section {
+                        ForEach(pastPeople.prefix(10)) { person in
+                            SelectPersonButton(person, fromHistory: true)
                         }
-                        .foregroundStyle(.foreground)
+                        
+                        if pastPeople.count > vm.historyPrefix {
+                            Button {
+                                withAnimation {
+                                    vm.updateHistoryPrefix()
+                                }
+                            } label: {
+                                Label("More", systemImage: "arrow.2.circlepath.circle")
+                            }
+                        }
+                    } header: {
+                        Text("History")
                     }
-                    
-                } header: {
-                    Text(getPlural())
-                } footer: {
-                    if !searchResults.isEmpty {
-                        TMDBAttributionView()
-                        .padding(.vertical)
-                        .padding(.bottom, 200) // in iOS 16 this adds extra space to the end to prevent the searchbox from overlapping the last item
+                } else {
+                    Section {
+                        ForEach(vm.searchResults) { person in
+                            SelectPersonButton(person, fromHistory: false)
+                        }
+                    } header: {
+                        Text(vm.getPlural())
+                    } footer: {
+                        if !vm.searchResults.isEmpty {
+                            TMDBAttributionView()
+                            .padding(.vertical)
+                            .padding(.bottom, 200) // in iOS 16 this adds extra space to the end to prevent the searchbox from overlapping the last item
+                        }
                     }
                 }
             }
             
             GroupBox {
                 Button("Search") {
-                    performSearch()
+                    vm.performSearch()
                     keyboardFocused = false
                 }
                 .accessibilityIdentifier("searchForPerson")
                 .buttonStyle(.borderedProminent)
                 .padding(.vertical, 5) // this evenly places the search button from the search box
                 
-                TextField("Enter Person Name", text: $query)
+                TextField("Enter Person Name", text: $vm.query)
                     .textFieldStyle(.roundedBorder)
                     .padding(.bottom, 40)
                     .focused($keyboardFocused)
@@ -65,15 +98,32 @@ struct SearchPersonView: View {
         .ignoresSafeArea(edges: keyboardFocused ? [] : [.bottom]) // MARK: without this the keyboard will overlap the text box
     }
     
-    func performSearch() {
-        Task {
-            searchResults = await tmdb.searchForPerson(with: query, pageNumber: 1) ?? []
-        }
-    }
     
-    func getPlural() -> String {
-        let count = searchResults.count
+}
+
+extension SearchPersonView {
+    @Observable
+    class viewModel {
+        let tmdb = TMDBAPI.shared
         
-        return "\(count) Result\(count == 0 || count > 2 ? "s" : "")"
+        var query: String = ""
+        var searchResults: [Person] = []
+        var historyPrefix: Int = 10
+        
+        func performSearch() {
+            Task {
+                searchResults = await tmdb.searchForPerson(with: query, pageNumber: 1) ?? []
+            }
+        }
+        
+        func getPlural() -> String {
+            let count = searchResults.count
+            
+            return "\(count) Result\(count == 0 || count > 2 ? "s" : "")"
+        }
+        
+        func updateHistoryPrefix() {
+            historyPrefix = historyPrefix + 10
+        }
     }
 }
